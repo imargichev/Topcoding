68b5f68ec09baf05118ac18c7cc8f8d261a617d0
Ivan Margichev <i.margichev@gmail.com>
2024-12-04 14:16:07 +0200

    Add method to count patterns appearing as substrings in a word
    - Implement `numOfStrings` method to count how many strings in the patterns array exist as substrings in the given word.
    - Add Javadoc comments with examples to explain the functionality.
    - Include a main method for testing the implementation.

---
+++ src/main/java/org/example/CountGoodTriplets.java
@@ -0,0 +1,51 @@
+package org.example;
+
+/**
+ * This class contains a method to count the number of good triplets in an array.
+ * A triplet (arr[i], arr[j], arr[k]) is considered good if the following conditions are met:
+ * 0 <= i < j < k < arr.length
+ * |arr[i] - arr[j]| <= a
+ * |arr[j] - arr[k]| <= b
+ * |arr[i] - arr[k]| <= c
+ * Where |x| denotes the absolute value of x.
+ */
+public class CountGoodTriplets {
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 1, 1, 9, 7};
+        int a = 7;
+        int b = 2;
+        int c = 3;
+
+        int result = countGoodTriplets(arr, a, b, c);
+        System.out.println(result);
+    }
+
+    /**
+     * Counts the number of good triplets in the given array.
+     *
+     * @param arr the array of integers
+     * @param a the maximum allowed difference between arr[i] and arr[j]
+     * @param b the maximum allowed difference between arr[j] and arr[k]
+     * @param c the maximum allowed difference between arr[i] and arr[k]
+     * @return the number of good triplets
+     */
+    public static int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int counter = 0;
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a) {
+                        if (Math.abs(arr[j] - arr[k]) <= b) {
+                            if (Math.abs(arr[i] - arr[k]) <= c) {
+                                System.out.println(arr[i] + "," + arr[j] + "," + arr[k]);
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/main/java/org/example/NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.HashSet;
+
+/**
+ * Given an array of strings patterns and a string word, this class contains a method to return the number of strings in patterns that exist as a substring in word.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ *
+ * Example 1:
+ * Input: patterns = ["a","abc","bc","d"], word = "abc"
+ * Output: 3
+ * Explanation:
+ * - "a" appears as a substring in "abc".
+ * - "abc" appears as a substring in "abc".
+ * - "bc" appears as a substring in "abc".
+ * - "d" does not appear as a substring in "abc".
+ * 3 of the strings in patterns appear as a substring in word.
+ *
+ * Example 2:
+ * Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+ * Output: 2
+ * Explanation:
+ * - "a" appears as a substring in "aaaaabbbbb".
+ * - "b" appears as a substring in "aaaaabbbbb".
+ * - "c" does not appear as a substring in "aaaaabbbbb".
+ * 2 of the strings in patterns appear as a substring in word.
+ *
+ * Example 3:
+ * Input: patterns = ["a","a","a"], word = "ab"
+ * Output: 3
+ * Explanation: Each of the patterns appears as a substring in word "ab".
+ */
+public class NumberOfStringsThatAppearAsSubstringsInWord {
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+
+        System.out.println(numOfStrings(patterns, word));
+    }
+
+    public static int numOfStrings(String[] patterns, String word) {
+        int counter = 0;
+        for (String pattern : patterns) {
+            if (word.contains(pattern)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
