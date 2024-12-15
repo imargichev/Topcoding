161233987adeac664d97883ea27a778a67b7bd36
Ivan Margichev <i.margichev@gmail.com>
2024-12-15 18:30:37 +0200

    n commit;

---
+++ src/main/java/org/example/CellsWithOddValuesInAMatrix.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class CellsWithOddValuesInAMatrix {
+    public static void main(String[] args) {
+
+    }
+    public static int oddCells(int m, int n, int[][] indices) {
+
+
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+
+
+        for (int[] index : indices) {
+            rows[index[0]]++;
+            cols[index[1]]++;
+        }
+
+        int oddCount = 0;
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if ((rows[i] + cols[j]) % 2 != 0) {
+                    oddCount++;
+                }
+            }
+        }
+
+        return oddCount;
+    }
+}
+
