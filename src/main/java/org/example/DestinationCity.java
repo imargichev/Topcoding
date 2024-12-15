14a5c5a9028e8ca857e13a23efc091ed54eff3be
Ivan Margichev <i.margichev@gmail.com>
2024-12-15 15:00:23 +0200

    n commit;

--- src/main/java/org/example/DestinationCity.java
+++ src/main/java/org/example/DestinationCity.java
@@ -1,5 +1,28 @@
 package org.example;
 
-public class DestinationCity {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class DestinationCity {
+    public static void main(String[] args) {
+
+    }
+    public static String destCity(List<List<String>> paths) {
+
+        Set<String> outgoingCities = new HashSet<>();
+
+
+        for (List<String> path : paths) {
+            outgoingCities.add(path.getFirst()); // Добавяме cityAi
+        }
+
+        for (List<String> path : paths){
+            String cityB = path.get(1);
+            if (!outgoingCities.contains(cityB)){
+                return cityB;
+            }
+        }
+        return "";
+    }
 }
