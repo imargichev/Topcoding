7ff2f49adc32e83d455d0fc52740540d405b2e90
Ivan Margichev <i.margichev@gmail.com>
2024-12-27 22:54:14 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,12 @@
+package org.example.strings;
+
+public class CheckIfTheSentenceIsPangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    private static boolean checkIfPangram(String sentence) {
+        return sentence.chars().distinct().count() == 26;
+    }
+}
---
+++ src/main/java/org/example/strings/FaultyKeyboard.java
@@ -0,0 +1,21 @@
+package org.example.strings;
+
+public class FaultyKeyboard {
+    public static void main(String[] args) {
+        String s = "string";
+        System.out.println(finalString(s));
+    }
+    public static String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
