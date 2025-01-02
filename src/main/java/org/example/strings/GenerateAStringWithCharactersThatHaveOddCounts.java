3394d11ee995badfe7840af3e8d58fae8334b89c
Ivan Margichev <i.margichev@gmail.com>
2025-01-02 14:58:23 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/GenerateAStringWithCharactersThatHaveOddCounts.java
@@ -0,0 +1,19 @@
+package org.example.strings;
+
+public class GenerateAStringWithCharactersThatHaveOddCounts {
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.println(generateTheString(n));
+    }
+    public static String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        if (n% 2 == 0 ){
+            sb.repeat('a',n-1);
+            sb.append('b');
+        }else {
+            sb.repeat('a',n);
+        }
+        return sb.toString();
+    }
+}
