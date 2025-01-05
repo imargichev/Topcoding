4e5f7906e42a13cbe56756f6e2017e21ec37e574
Ivan Margichev <i.margichev@gmail.com>
2025-01-05 18:34:04 +0200

    n commit;a

--- src/main/java/org/example/strings/MinimizeStringLength.java
+++ src/main/java/org/example/strings/MinimizeStringLength.java
@@ -1,31 +1,20 @@
 package org.example.strings;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MinimizeStringLength {
     public static void main(String[] args) {
-        String s = "baadccab";
+        String s = "cbbd";
         System.out.println(minimizedStringLength(s));
     }
     public static int minimizedStringLength(String s) {
-        Map<Character,Integer> storeChars = new TreeMap<>();
+        char[] chars = s.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (storeChars.containsKey(currentChar)) {
-                storeChars.put(currentChar, storeChars.get(currentChar) + 1);
-            } else {
-                storeChars.put(currentChar, 1);
-            }
+        Set<Character> uniqueChars = new HashSet<>();
+        for (char c : chars) {
+            uniqueChars.add(c);
         }
-        int counter = 0;
-        for(Map.Entry<Character, Integer> c : storeChars.entrySet()){
-            if (c.getValue() <=1){
-                storeChars.put(c.getKey(), c.getValue() - 1);
-                counter++;
-            }
-        }
-        return s.length() - counter;
+
+        return uniqueChars.size();
     }
 }
