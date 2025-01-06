ca1fe85c6201a5f250ab86ad802a3cead1aa5b78
Ivan Margichev <i.margichev@gmail.com>
2025-01-06 20:12:45 +0200

    n commit;a

--- src/main/java/org/example/strings/UncommonWordsFromTwoSentences.java
+++ src/main/java/org/example/strings/UncommonWordsFromTwoSentences.java
@@ -1,11 +1,13 @@
 package org.example.strings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UncommonWordsFromTwoSentences {
     public static void main(String[] args) {
-        String s1 = "this apple is sweet", s2 = "this apple is sour";
+        String s1 = "apple apple", s2 = "banana";
         String[] finalResult = uncommonFromSentences(s1,s2);
 
         for (String s : finalResult){
@@ -14,23 +16,32 @@ public class UncommonWordsFromTwoSentences {
 
     }
     public static String[] uncommonFromSentences(String s1, String s2) {
-
+        //create map for store;
+        Map<String, Integer> wordCount = new HashMap<>();
+        //expected result
         List<String> results = new ArrayList<>();
 
+        //here we split the words;
         String[] array1 = s1.split(" ");
         String[] array2 = s2.split(" ");
 
-        for (int i = 0; i < array1.length ; i++) {
-            if (!s1.contains(array2[i])){
-                results.add(array2[i]);
-            }
+        for (String word : array1) {
+            //for each word in array we put that into hashMap, and we increase the value;
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
-        for (int i = 0; i < array1.length ; i++) {
-            if (!s2.contains(array1[i])){
-                results.add(array1[i]);
+
+        for (String word : array2) {
+            //for each word in array we put that into hashMap, and we increase the value;
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            //check each value if we have only once
+            if (entry.getValue() == 1) {
+                results.add(entry.getKey());
             }
         }
 
-       return
+        return results.toArray(new String[0]);
     }
 }
