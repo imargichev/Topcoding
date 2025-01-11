4612f7429203616b2d4c41e7151ac97f95c24d01
Ivan Margichev <i.margichev@gmail.com>
2025-01-11 15:31:14 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -0,0 +1,20 @@
+/*
+package org.example.strings;
+
+public class SubstringsOfSizeThreeWithDistinctCharacters {
+    public static void main(String[] args) {
+        String s = "xyzzaz";
+        System.out.println(countGoodSubstrings(s));
+    }
+
+    public static int countGoodSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i+=2) {
+            for (int j = i; j < ; j++) {
+
+            }
+        }
+        return count;
+    }
+}
+*/
