16cf1a0b766a26e675a07b5d09d826afbb6dcc73
Ivan Margichev <i.margichev@gmail.com>
2025-01-11 18:20:54 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,21 @@
+package org.example.strings;
+
+public class DeleteColumnsToMakeSorted {
+    public static void main(String[] args) {
+        String[] strs = {"cba","daf","ghi"};
+        System.out.println(minDeletionSize(strs));
+    }
+    public static int minDeletionSize(String[] strs){
+        int count = 0;
+
+        for (int j = 0; j < strs[0].length(); j++) {
+            for (int i = 0; i < strs.length - 1; i++) {
+                if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
