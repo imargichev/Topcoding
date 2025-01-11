5663e9c5fdfef9d35fd4548f842aefb56d34be78
Ivan Margichev <i.margichev@gmail.com>
2025-01-11 22:01:14 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/FindCommonCharacters.java
@@ -0,0 +1,37 @@
+package org.example.strings;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindCommonCharacters {
+    public static void main(String[] args) {
+        String[] words = {"bella","label","roller"};
+        commonChars(words);
+    }
+    public static List<String> commonChars(String[] words) {
+        Map<Character, Integer> pairChars = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        int size = words.length;
+
+        for (char c : words[0].toCharArray()) {
+            pairChars.put(c, pairChars.getOrDefault(c, 0) + 1);
+            int count = pairChars.values()
+            if (count >= size) {
+                result.add(pairChars.k);
+            }
+        }
+
+
+        for (int count : pairChars.values()) {
+            if (count >= size) {
+                result.add(pairChars.k);
+            }
+        }
+
+        return pairChars.size() == size;
+
+
+    }
+}
