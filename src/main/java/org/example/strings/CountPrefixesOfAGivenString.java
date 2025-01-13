8126af92c04f16453e56233461a23802056c5b03
Ivan Margichev <i.margichev@gmail.com>
2025-01-13 16:36:15 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/CountPrefixesOfAGivenString.java
@@ -0,0 +1,30 @@
+package org.example.strings;
+
+import java.util.ArrayList;
+
+public class CountPrefixesOfAGivenString {
+    public static void main(String[] args) {
+        String [] words = {"a","b","c","ab","bc","abc"};
+        String s = "abc";
+        System.out.println(countPrefixes(words,s));
+
+    }
+    public static int countPrefixes(String[] words, String s) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> allCombo = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            allCombo.add(sb.toString());
+        }
+
+        for(String str : words){
+            if (allCombo.contains(str)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
