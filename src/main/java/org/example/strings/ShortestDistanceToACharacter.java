71ed93b0838a62e55e2092a4ec4ba94cb1b75484
Ivan Margichev <i.margichev@gmail.com>
2025-01-17 21:16:52 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/ShortestDistanceToACharacter.java
@@ -0,0 +1,34 @@
+/*
+package org.example.strings;
+
+public class ShortestDistanceToACharacter {
+    public static void main(String[] args) {
+       String s = "loveleetcode";
+       char c = 'e';
+       int [] result = shortestToChar(s,c);
+    }
+    public static int[] shortestToChar(String s, char c) {
+        int [] result = new int[s.length()];
+        int indx = 0;
+        int left = 0;
+        int right = 0;
+        boolean isDone = true;
+        do {
+            s.charAt(indx);
+
+
+            if (s.charAt(indx) == c){
+                while (isDone){
+
+                result[indx - left] =left++;
+                result[indx + right] =right++;
+                    isDone = false;
+                    isDone =
+                }
+            }
+            indx++;
+
+        }while (s.length()-1 == )
+    }
+}
+*/
