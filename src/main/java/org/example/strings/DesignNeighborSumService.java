573e96af7991227560ea63a11b6c6e6433a9d597
Ivan Margichev <i.margichev@gmail.com>
2025-01-22 13:51:27 +0200

    n commit;a

---
+++ src/main/java/org/example/strings/DesignNeighborSumService.java
@@ -0,0 +1,24 @@
+package org.example.strings;
+
+public class DesignNeighborSumService {
+    public static void main(String[] args) {
+        String [] input = {"NeighborSum", "adjacentSum", "adjacentSum", "diagonalSum", "diagonalSum"};
+        int[][] gridche = {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8}};
+    }
+    public static void NeighborSum(int[][] grid) {
+
+    }
+
+    public static int adjacentSum(int value) {
+
+        return value;
+    }
+
+    public static int diagonalSum(int value) {
+
+        return value;
+    }
+}
