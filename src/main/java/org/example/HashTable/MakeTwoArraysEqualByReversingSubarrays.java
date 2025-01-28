be5c0690cc870b7d203cae16d3bb63b2bff876a4
Ivan Margichev <i.margichev@gmail.com>
2025-01-28 22:34:54 +0200

    n commit;a

--- src/main/java/org/example/HashTable/MakeTwoArraysEqualByReversingSubarrays.java
+++ src/main/java/org/example/HashTable/MakeTwoArraysEqualByReversingSubarrays.java
@@ -10,15 +10,6 @@ public class MakeTwoArraysEqualByReversingSubarrays {
     public static boolean canBeEqual(int[] target, int[] arr) {
         Arrays.sort(target);
         Arrays.sort(arr);
-        if (target.length!=arr.length){
-            return false;
-        }else {
-            for (int i = 0; i < target.length; i++) {
-                if (target[i]!=arr[i]){
-                    return false;
-                }
-            }
-        }
-        return true;
+        return Arrays.equals(target, arr);
     }
 }
