c355162cde1449f0809d637458d70ceb9be66ff3
Ivan Margichev <i.margichev@gmail.com>
2025-02-13 11:41:21 +0200

    n commit;a

--- src/main/java/org/example/BinarySearch/FindTargetIndicesAfterSortingArray.java
+++ src/main/java/org/example/BinarySearch/FindTargetIndicesAfterSortingArray.java
@@ -21,6 +21,7 @@ public class FindTargetIndicesAfterSortingArray {
         // Perform binary search to find the first occurrence of the target
         int left = 0;
         int right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] < target) {
