57785de5239c7fa03cc9efbe2e4c3f9e18f76188
Ivan Margichev <i.margichev@gmail.com>
2025-02-27 19:21:03 +0200

    n commit;a

---
+++ src/main/java/org/example/BinarySearch/GuessNumberHigherOrLower.java
@@ -0,0 +1,35 @@
+package org.example.BinarySearch;
+
+public class GuessNumberHigherOrLower {
+    public static void main(String[] args) {
+       int n = 10;
+       int pick = 6;
+        System.out.println(guessNumber(n));
+
+    }
+    public static int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if (guess(mid) == 0){
+                return mid;
+            } else if (guess(mid) == 1){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+   public static int guess(int num){
+        if (num == pick){
+            return 0;
+        } else if (num < pick){
+            return 1;
+        } else {
+            return -1;
+        }
+   }
+}
